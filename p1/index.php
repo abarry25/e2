298e1957de55632f76b9db86e00341aba6a73f68
Alexa Barry <alexa_barry@harvard.edu>
2021-09-10 02:24:08 +0000

    Initiated p1

---
+++ p1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> Project 1 </title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1>Project 1</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+        <li>...</li>
+    </ul>
+
+    <h2>Results</h2>
+    <ul>
+        <li>...</li>
+    </ul>
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
