62cbdd99a058da6b6ddad9f64bd1dc62814dbbec
Alexa Barry <alexa_barry@harvard.edu>
2021-09-29 02:50:22 +0000

    Beginning Pass the Pigs

---
+++ p1/pass_the_pigs.php
@@ -0,0 +1,20 @@
+<?php
+
+//arrays for game, possible positions for pig rolls
+
+$pig_position = [
+    'left',
+    'right',
+    'standing',
+    'back',
+    'leaning',
+    'snout', ];
+
+//array for value of each possible possition
+$position_values = [
+    'right'=> 0,
+    'standing'=> 5,
+    'back'=> 5,
+    'leaning'=> 15,
+    'snout' => 10,
+];
