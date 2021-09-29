38a61b0d7c20d0dde89f038f3236a813ca3be8bb
Alexa Barry <alexa_barry@harvard.edu>
2021-09-29 02:56:09 +0000

    defining variables for game

--- p1/pass_the_pigs.php
+++ p1/pass_the_pigs.php
@@ -18,3 +18,15 @@ $position_values = [
     'leaning'=> 15,
     'snout' => 10,
 ];
+
+//variables settings pig rolls to generate random number
+$pig_a = rand(0,5);
+$pig_b = rand(0,5);
+
+//variables randomly generating a pig position for each rolled pig
+$pig_a_position = $pig_position[$pig_a];
+$pig_b_position = $pig_position[$pig_b];
+
+// var_dump($pig_a_position);
+
+//player score has to start at 0
