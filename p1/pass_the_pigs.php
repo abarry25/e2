b44da2a1f6b5e7418b029a3acf8585c2a1b492a9
Alexa Barry <alexa_barry@harvard.edu>
2021-09-30 20:04:46 +0000

    adding pig positions + values to player 1

--- p1/pass_the_pigs.php
+++ p1/pass_the_pigs.php
@@ -30,3 +30,37 @@ $pig_b_position = $pig_position[$pig_b];
 // var_dump($pig_a_position);
 
 //player score has to start at 0
+$player1_score = 0;
+$player2_score = 0;
+
+if ($pig_a_position == $pig_b_position) {
+    if (($pig_a_position == 'left' and $pig_b_position = 'left') or ($pig_a_position == 'right' and $pig_b_position == 'left')) {
+        $player1_score += 1;
+        $read_out = 'Sider!';
+        var_dump($read_out);
+        var_dump($player1_score);
+    } elseif ($pig_a_position == 'leaning' and $pig_b_position == 'leaning') {
+        $player1_score += 60;
+        $read_out = 'Double Leaning Jowler!';
+        var_dump($read_out);
+        var_dump($player1_score);
+    } elseif ($pig_a_position == 'back' and $pig_b_position == 'back') {
+        $player1_score += 20;
+        $read_out = 'Double Razorback';
+        var_dump($read_out);
+        var_dump($player1_score);
+    } elseif ($pig_a_position == 'standing' and $pig_b_position == 'standing') {
+        $player1_score += 20;
+        $read_out = 'Double Trotter!';
+        var_dump($read_out);
+        var_dump($player1_score);
+    } elseif ($pig_a_position == 'snout' and $pig_b_position == 'snout') {
+        $player1_score += 20;
+        $read_out = 'Double Snouter!';
+        var_dump($read_out);
+        var_dump($player1_score);
+    } 
+};
+
+//roll type + score if the pig positions are not the same!//
+if ($pig_a_position  != $pig_b_position) {
